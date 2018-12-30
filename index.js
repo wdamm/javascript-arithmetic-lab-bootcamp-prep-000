bfbf84ea2dde6baadb7ccfb801b9b3dc2d58ac3b
William Damm <wdamm@powerhrg.com>
2018-12-30 04:04:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,11 +23,10 @@ function dec(n) {
 }
 
 function makeInt(n,o) {
-<<<<<<< HEAD
+
   return(parseInt(n))
 =======
   return(parseInt(n),parseInt(n*0))
->>>>>>> 2816e1f34803a5133728740986d24f44caa8092e
 }
 
 function preserveDecimal(n) {
